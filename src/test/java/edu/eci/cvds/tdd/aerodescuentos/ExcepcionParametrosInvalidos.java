b393da9d8aff34ccc683a2a9c7df174ea2fcfb45
sergiolopezzl <slopez1660@gmail.com>
2023-09-06 15:19:41 -0500

    cambios

--- src/test/java/edu/eci/cvds/tdd/aerodescuentos/ExcepcionParametrosInvalidos.java
+++ src/test/java/edu/eci/cvds/tdd/aerodescuentos/ExcepcionParametrosInvalidos.java
@@ -1,11 +1,13 @@
+package edu.eci.cvds.tdd.aerodescuentos;
+
 /**
  *Clase de excepciones para aerodescuentoa
  */
 public class ExcepcionParametrosInvalidos extends Exception
 {
-    public static final String E1 = "";
-    public static final String E2 = "";
-    public static final String E23 = "";
+    public static final String NEGATIVA_CERO = "Si la tarifaBase es negativa o igual a cero.";
+    public static final String DIAS_NEGATIVOS = "Si los diasAntelacion son negativos.";
+    public static final String EDAD_NEGATIVA = "Si la edad es negativa.";
     
     public ExcepcionParametrosInvalidos(String mensaje){
         super(mensaje);
