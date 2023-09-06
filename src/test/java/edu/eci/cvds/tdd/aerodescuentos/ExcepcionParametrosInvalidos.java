3c45a00b42529d602c42cd705a262b6be0338f26
Andres Felipe <dinho2001.7@gmail.com>
2023-09-06 15:10:38 -0500

    Clase de excepcicones

---
+++ src/test/java/edu/eci/cvds/tdd/aerodescuentos/ExcepcionParametrosInvalidos.java
@@ -0,0 +1,13 @@
+/**
+ *Clase de excepciones para aerodescuentoa
+ */
+public class ExcepcionParametrosInvalidos extends Exception
+{
+    public static final String E1 = "";
+    public static final String E2 = "";
+    public static final String E23 = "";
+    
+    public ExcepcionParametrosInvalidos(String mensaje){
+        super(mensaje);
+    }
+}
