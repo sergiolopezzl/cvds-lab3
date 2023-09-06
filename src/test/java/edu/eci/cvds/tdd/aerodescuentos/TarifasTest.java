49e1997e1c593335458478be03cc7a89be8bdbaf
sergiolopezzl <slopez1660@gmail.com>
2023-09-06 15:56:22 -0500

    cambios

--- src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -2,52 +2,86 @@ package edu.eci.cvds.tdd.aerodescuentos;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos;
-//import edu.eci.cvds.tdd.aerodescuentos.ExcepcionParametrosInvalidos;
 
 public class TarifasTest {
 
-    private CalculadorDescuentos calculadorDescuentos;
-
     @Before
     public void setUp() {
-        calculadorDescuentos = new CalculadorDescuentos();
+        
     }
 
     @Test
-    public void testCalculoTarifaDescuentoMenor18() {
-        long tarifaBase = 100;
+    public void testDescuentoMayor20DiasEdadEntre18y65() {
+        double tarifaBase = 100;
         int diasAntelacion = 25;
-        int edad = 10;
-        try {
-            long resultado = calculadorDescuentos.calculoTarifa(tarifaBase, diasAntelacion, edad);
-            // El resultado esperado es una tarifa con descuento del 20% (15% + 5%) sobre la tarifa base.
-            Assert.assertEquals(80, resultado);
-        } catch (ExcepcionParametrosInvalidos e) {
-            Assert.fail("No debería lanzar una excepción en este caso.");
-        }
+        int edad = 30;
+        double resultado = CalculadorDescuentos.calculoTarifa(tarifaBase, diasAntelacion, edad);
+        CalculadorDescuentos.calculoTarifa(tarifaBase, diasAntelacion, edad);
+        // Resultado esperado: 15% de descuento sobre 100.
+        Assert.assertEquals(85, resultado, 1);
     }
-
-    @Test(expected = ExcepcionParametrosInvalidos.class)
-    public void testExcepcionParametrosInvalidosTarifaNegativa() throws ExcepcionParametrosInvalidos {
-        long tarifaBase = -50;
-        int diasAntelacion = 30;
-        int edad = 70;
-        calculadorDescuentos.calculoTarifa(tarifaBase, diasAntelacion, edad);
-    }
-
+    
     @Test
-    public void testCalculoTarifaSinDescuento() {
-        long tarifaBase = 100;
+    public void testDescuentoMayor20DiasEdadMenor18() {
+        double tarifaBase = 100;
+        int diasAntelacion = 25;
+        int edad = 16;
+        double resultado = CalculadorDescuentos.calculoTarifa(tarifaBase, diasAntelacion, edad);
+        // Resultado esperado: 15% + 5% de descuento sobre 100.
+        Assert.assertEquals(80, resultado, 1);
+    }
+    
+    @Test
+    public void testDescuentoMayor20DiasEdadMayor65() {
+        double tarifaBase = 100;
+        int diasAntelacion = 25;
+        int edad = 70;
+        double resultado = CalculadorDescuentos.calculoTarifa(tarifaBase, diasAntelacion, edad);
+        // Resultado esperado: 15% + 8% de descuento sobre 100.
+        Assert.assertEquals(77, resultado,1);
+    }
+    
+    @Test
+    public void testSinDescuentoMenorIgual20DiasEdadEntre18y65() {
+        double tarifaBase = 100;
         int diasAntelacion = 10;
         int edad = 30;
-        try {
-            long resultado = calculadorDescuentos.calculoTarifa(tarifaBase, diasAntelacion, edad);
-            // No hay descuento en este caso.
-            Assert.assertEquals(100, resultado);
-        } catch (ExcepcionParametrosInvalidos e) {
-            Assert.fail("No debería lanzar una excepción en este caso.");
-        }
+        double resultado = CalculadorDescuentos.calculoTarifa(tarifaBase, diasAntelacion, edad);
+        // Resultado esperado: Sin descuento, tarifa base intacta.
+        Assert.assertEquals(100, resultado,1);
+    }
+    
+    // Agrega casos de prueba para las clases de equivalencia restantes y las condiciones de frontera.
+    
+
+    @Test
+    public void testCalculoTarifaDescuentoMenor18() throws ExcepcionParametrosInvalidos {
+        double tarifaBase = 100;
+        int diasAntelacion = 25;
+        int edad = 16;
+        double resultado = CalculadorDescuentos.calculoTarifa(tarifaBase, diasAntelacion, edad);
+        // El resultado esperado es una tarifa con descuento del 20% (15% + 5%) sobre la tarifa base.
+        Assert.assertEquals(80, resultado,1);
+    }
+
+    /*
+    @Test(expected = ExcepcionParametrosInvalidos.class)
+    public void testExcepcionParametrosInvalidosTarifaNegativa() throws ExcepcionParametrosInvalidos {
+        double tarifaBase = -50;
+        int diasAntelacion = 30;
+        int edad = 70;
+        CalculadorDescuentos.calculoTarifa(tarifaBase, diasAntelacion, edad);
+    }
+    */
+
+    @Test
+    public void testCalculoTarifaSinDescuento() throws ExcepcionParametrosInvalidos{
+        double tarifaBase = 100;
+        int diasAntelacion = 10;
+        int edad = 30;
+        double resultado = CalculadorDescuentos.calculoTarifa(tarifaBase, diasAntelacion, edad);
+        // No hay descuento en este caso.
+        Assert.assertEquals(100, resultado, 1);
     }
 
     // Agrega más pruebas según tus casos de equivalencia y condiciones límite.
